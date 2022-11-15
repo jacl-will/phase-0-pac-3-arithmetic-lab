7a82ac61ccfa61c272b1084e87414bcd4d2613be
Jaclyn Williams <jaclyn.williams143@gmail.com>
2022-11-15 12:29:02 -0500

    commit tree f5db6d5a62e5606cf6d7b83837702c952d6451ba

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a, b) {
+return a + b
+}
+
+function subtract(a, b) {
+ return a - b
+}
+
+function multiply(a, b) {
+return a * b
+}
+
+function divide(a, b) {
+return a / b
+}
+
+function increment(a) {
+    return ++a
+}
+
+function decrement(a){
+    return --a
+}
+
+function makeInt(a) {
+    return a
+}
+
+function makeInt(a) {
+ return (parseInt (a, 10))
+
+}
+
+
+/*function makeInt('0x2328') {
+    return 0
+} note to self: having a difficult time with this one*/
+
+function preserveDecimal(a) {
+    return parseFloat(a, 10)
+}
